fecd312fe9fe5715f97f25e12463f7957568692a
jsan <ahsan.wtc@gmail.com>
2021-10-14 16:11:21 +0200

    initial commit

---
+++ index.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * Plugin Name: MCQs Plugin
+ * Plugin URI: https://github.com/ahsanwtc/mcqs-plugin
+ * Description: A simple plugin for adding mcqs blocks to posts.
+ * Version: 1.0
+ * Author: jsan
+ * Author URI: https://iamahsan.dev
+ */
+
+if (!defined('ABSPATH')) exit; // Exit if accessed directly
+
+class MCQsPlugin {
+  function __construct () {
+    add_action('enqueue_block_assets', array($this, 'adminAssets'));
+  }
+
+  function adminAssets () {
+    wp_enqueue_script('ournewblocktype', plugin_dir_url(__FILE__) . 'test.js', array('wp-blocks'));
+  }
+}
+
+$mcqsPlugin = new MCQsPlugin();
