b3dc023b90895fe0519fb9e61b3b09bc81f48980
jsan <ahsan.wtc@gmail.com>
2021-10-14 16:39:47 +0200

    adding ui block

--- test.js
+++ test.js
@@ -1,1 +1,11 @@
-alert('Hello world');
+wp.blocks.registerBlockType('mcqs-plugin/mcqs', {
+  title: 'MCQs',
+  icon: 'smiley',
+  category: 'common',
+  edit: () => {
+    return wp.element.createElement('h3', null, 'Hello this is from admin editor screen');
+  },
+  save: () => {
+    return wp.element.createElement('h3', null, 'Hello this is frontend');
+  }
+});
