e7bba0a9a8e9bd32b5fb1f636084acd3215ab3e3
jsan <ahsan.wtc@gmail.com>
2021-10-14 19:18:58 +0200

    adding attributes

--- src/index.js
+++ src/index.js
@@ -2,20 +2,37 @@ wp.blocks.registerBlockType('mcqs-plugin/mcqs', {
   title: 'MCQs',
   icon: 'smiley',
   category: 'common',
-  edit: () => {
+  attributes: {
+    sky: {
+      type: 'string',
+    },
+    grass: {
+      type: 'string',
+    }
+  },
+  edit: props => {
+    const { attributes: { sky, grass }} = props;
+
+    const updateSkyColor = e => {
+      props.setAttributes({ sky: e.target.value });
+    };
+
+    const updateGrassColor = e => {
+      props.setAttributes({ grass: e.target.value });
+    };
+
     return (
       <div>
-        <p>Hello, this is paragraph</p>
-        <h4>Hi there</h4>
+        <input type="text" placeholder="sky color" value={sky} onChange={updateSkyColor} />
+        <input type="text" placeholder="grass color" value={grass} onChange={updateGrassColor} />
       </div>
     );
   },
-  save: () => {
+  save: props => {
+    const { attributes: { sky, grass }} = props;
+
     return (
-      <React.Fragment>
-        <h5>frontend</h5>
-        <h2>frontend</h2>
-      </React.Fragment>
+      <p>Today sky is {sky} and the grass is {grass}.</p>
     );
   }
 });
